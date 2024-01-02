6d9e3aa3dc54c8242a9ddfd2237e3ca7980b0db9
justrunis <justinas.runevicius@ktu.edu>
2024-01-02 19:13:47 +0200

    map function for notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import Header from './Header';
-import Note from './Note';
-import Footer from './Footer';
-const App = () => {
-    return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    );
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Note = () => {
-    return (
-        <div className='note'>
-            <h1>Note title</h1>
-            <p>Note text</p>
-        </div>
-    );
-};
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
 
 export default Note;
