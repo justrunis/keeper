a90fac66617cd99fa39559222fb1f37a2002ec00
justrunis <justinas.runevicius@ktu.edu>
2024-01-02 17:30:28 +0200

    commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Note = () => {
+    return (
+        <div className='note'>
+            <h1>Note title</h1>
+            <p>Note text</p>
+        </div>
+    );
+};
+
+export default Note;
